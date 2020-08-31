29f78b206727ed013418e43a8063601a42b0fff9
Kevin Barreto <barretokevin@hotmail.com>
2020-08-31 09:20:34 -0500

    String Methods in JS

---
+++ string_methods.js
@@ -0,0 +1,16 @@
+let msg = "   You are so Grounded Mister";
+msg.toUpperCase();
+msg.toLowerCase();
+msg.trim();
+///////////////////////////////////////////
+let tvShow = "catdog";
+tvShow.indexOf("cat");
+tvShow.indexOf("dog");
+tvShow.indexOf("entertainment");
+/////////////////////////////////////////
+let str = "hellodopaliciousstuff";
+str.slice(0, 5);
+str.slice(4);
+///////////////////////////////
+let annoyingLaugh = "teheee so funny looolz";
+annoyingLaugh.replace("teheee", "hahaha");
