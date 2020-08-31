4c090e542b9635b95b7d34be6db8b5ba39e3c67f
Kevin Barreto <barretokevin@hotmail.com>
2020-08-31 09:57:03 -0500

    Math Object in JS

---
+++ math_object.js
@@ -0,0 +1,12 @@
+const pi = Math.PI;
+pi;
+
+Math.round(10.35);
+Math.round(10.87);
+Math.pow(2, 8);
+Math.floor(3.678);
+Math.ceil(10.4567);
+Math.random();
+// generate random number from 1 - 20
+let randomNum = Math.floor(Math.random() * 20) + 1;
+randomNum;
