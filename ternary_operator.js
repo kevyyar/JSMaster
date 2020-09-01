dfbe6ef5cb9dc84048ca8147e7d14998ebf87e52
Kevin Barreto <barretokevin@hotmail.com>
2020-09-01 09:38:42 -0500

    Another alternative for short If-Else Statements

---
+++ ternary_operator.js
@@ -0,0 +1,12 @@
+// condition ? expIfTrue : expIfFalse
+
+let num = 13;
+
+num === 7 ? "Lucky number!" : "That's not a lucky number";
+
+let status = "offline";
+let color = status === "offline" ? "red" : "green";
+
+let age = 18;
+
+age === 18 ? "You're allowed to enter" : "You're not old enough to enter";
