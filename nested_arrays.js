6cd9b6340139642fa1d99387c25492e55d8f5e61
Kevin Barreto <barretokevin@hotmail.com>
2020-09-03 15:01:45 -0500

    Some examples with nested arrays

---
+++ nested_arrays.js
@@ -0,0 +1,11 @@
+const board = [
+  ["O", null, "X"],
+  [null, "X", "O"],
+  ["X", "O", null],
+];
+
+const englishGroups = [
+  ["Mary", "Joe", "Eddi"],
+  ["Kev", "Bryan", "Kathy"],
+  ["Yuyu", "Lopea", "Colt"],
+];
