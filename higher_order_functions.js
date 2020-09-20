1d781f0f9b8c587db5557420ea2e1a4289bb244c
Kevin Barreto <barretokevin@hotmail.com>
2020-09-20 18:39:56 -0500

    how to return functions and use functions within functions

---
+++ higher_order_functions.js
@@ -0,0 +1,31 @@
+function add(x, y) {
+  return x + y;
+}
+
+const subtract = function (x, y) {
+  return x - y;
+};
+
+function multiply(x, y) {
+  return x * y;
+}
+
+const divide = function (x, y) {
+  return x / y;
+};
+
+const operations = [add, subtract, multiply, divide];
+
+for (let func of operations) {
+  let result = func(3, 4);
+  console.log(result);
+}
+
+const math = {
+  sum: add,
+  minus: subtract,
+  times: multiply,
+  division: divide,
+};
+console.log(math.sum(3, 5));
+
